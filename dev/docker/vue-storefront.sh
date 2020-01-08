7940cb18eafc32205112cbe4c56309215ffd3218
mkoszutowski <mkoszutowski@divante.pl>
2020-01-08 13:38:53 +0100

    Check deploy checker

--- dev/docker/vue-storefront.sh
+++ dev/docker/vue-storefront.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-for i in {1..100}
+for i in {1..120}
 do
    printf "For those saying \"it works for me\", the behavior of echo varies quite a bit between versions. Some will even print the \"-e\" as part of their output. If you want predictable behavior for anything nontrivial, use printf instead (as in @sth's answer). – Gordon Davisson Dec 12 '11 at 1:58\n"
    sleep 1
